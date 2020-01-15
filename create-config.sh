4f8502890f587641dd465f1751adf152822766f1
Jenkins <jenkins@jenkins.prod.sdp.aa.liberty.biz>
2020-01-15 14:37:19 +0000

    Create environment monitoring-uat-onzeprj-goodapp-nifi

--- create-config.sh
+++ create-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook playbooks/create-new-environment.yml -e "data_domain=automation resource_group_phase=uat project_name=onzeprj application_name=goodapp env_type=nifi sdp_admin_public=~/.ssh/sdp_pub project_description='Good project'"
+ansible-playbook playbooks/create-new-environment.yml -e "data_domain=monitoring resource_group_phase=uat project_name=onzeprj application_name=goodapp env_type=nifi sdp_admin_public=~/.ssh/sdp_pub project_description='Good project'"
 
