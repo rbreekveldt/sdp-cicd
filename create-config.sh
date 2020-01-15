afd5da1e250909b29e11fdcb2e2ae5085f31a36b
Jenkins <jenkins@jenkins.prod.sdp.aa.liberty.biz>
2020-01-15 14:13:57 +0000

    Create environment /var/lib/jenkins/jobs/CICD Automation/dp-uat-onzeprj-goodapp-nifi

--- create-config.sh
+++ create-config.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ansible-playbook playbooks/create-new-environment.yml -e "data_domain=dp resource_group_phase=uat project_name=onzeprj application_name=goodapp env_type=nifi sdp_admin_public=~/.ssh/sdp_pub"
+ansible-playbook playbooks/create-new-environment.yml -e "data_domain=dp resource_group_phase=uat project_name=onzeprj application_name=goodapp env_type=nifi sdp_admin_public=~/.ssh/sdp_pub project_description='Good project'"
+
