b8d123dc2b330872ccef2c05c305cf6012020b92
Hakan Elgin <helgin.contractor@libertyglobal.com>
2020-01-24 14:04:33 +0100

    no escape

--- playbooks/roles/calculate-vnet-prefix/files/calculate_vnet_prefix.py
+++ playbooks/roles/calculate-vnet-prefix/files/calculate_vnet_prefix.py
@@ -7,7 +7,7 @@ def find_next_prefix():
   third_digit = 0
   
   #Read all vnet prefix in list
-  with fileinput.input(files=('/var/lib/jenkins/workspace/CICD\ Automation/Create\ Data\ Environment/playbooks/roles/calculate-vnet-prefix/files/prefixfile')) as f:
+  with fileinput.input(files=('/var/lib/jenkins/workspace/CICD Automation/Create Data Environment/playbooks/roles/calculate-vnet-prefix/files/prefixfile')) as f:
     for line in f:
       token = line[0:(line.rindex('.'))]
       lineList.append(token)
