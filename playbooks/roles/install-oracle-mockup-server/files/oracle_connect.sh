ab3a01e3a88af6878921911d8e5a6e0f7690f337
Mike De Kok <kokm@192.168.2.15>
2020-04-01 10:02:46 +0200

    fix role

--- playbooks/roles/install-oracle-mockup-server/files/oracle_connect.sh
+++ playbooks/roles/install-oracle-mockup-server/files/oracle_connect.sh
@@ -9,5 +9,4 @@ CREATE USER sdp_user IDENTIFIED BY "sdppwd" DEFAULT TABLESPACE "sdp_db" TEMPORAR
 
 GRANT CONNECT, RESOURUCE TO sdp_user;
 
-"jdbc:oracle:thin:@vm-x-sit-oracle01.internal.cloudapp.net:1521/ORCL", "sdp_user", "sdppwd"
 EOF
