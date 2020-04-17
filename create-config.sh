7474308363376bc984e6daf40ffcfdbb136dbe77
Remco Breekveldt <rbreekveldt.contractor@libertyglobal.com>
2020-04-17 15:30:19 +0200

    removed application_name

--- create-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ansible-playbook playbooks/create-new-environment.yml -e "data_domain=monitoring resource_group_phase=uat project_name=secuPI application_name=badapp env_type=nifi sdp_admin_public=~/.ssh/sdp_pub project_description='Bad project'"
-
